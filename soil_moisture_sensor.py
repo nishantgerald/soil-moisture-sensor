e81381af35418f9ba4aa3d207d19196f8bd59649
Nishant Gerald <nishant.gerald@gmail.com>
2023-07-03 10:17:19 -0400

    Sending sensor data to API

--- soil_moisture_sensor.py
+++ soil_moisture_sensor.py
@@ -1,8 +1,13 @@
 from machine import ADC
+import json
 import time
+import urequests
+import ujson
 
 # Initialize the ADC
 adc = ADC(0)
+
+url = "http://sensor.nishantgerald.com/api/sensor"
 
 def map_value(value, in_min, in_max, out_min, out_max):
     # Map a value from one range to another
@@ -13,8 +18,20 @@ while True:
     moisture_value = adc.read()
 
     # Map the value to a moisture percentage and invert it
-    moisture_percentage = map_value(moisture_value, 450, 850, 100, 0)
+    moisture_percentage = map_value(moisture_value, 400, 850, 100, 0)
+    
+    data = {
+        "value": moisture_percentage
+    }
+    
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    
+    response = urequests.put(url, data=ujson.dumps(data), headers=headers)
+    
+    print(response.text)
     
     # print("Soil moisture level (%): ", moisture_percentage)
     print("Soil moisture level (%): ", moisture_percentage)
-    time.sleep(1)
+    time.sleep(2)
